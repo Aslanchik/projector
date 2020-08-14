ce81670286a2224539fb158bf55179699781fc57
aslanchik <aslanchikgw2@hotmail.com>
2020-08-14 14:03:14 +0300

    worked out how to pull one document at a time with firebase in the project details component

--- src/components/projects/ProjectDetails.jsx
+++ src/components/projects/ProjectDetails.jsx
@@ -1,32 +1,41 @@
 import React from "react";
 
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+
 const ProjectDetails = (props) => {
-  const {
-    match: {
-      params: { id },
-    },
-  } = props;
-  return (
+  const { project } = props;
+
+  return project ? (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title - {id}</span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
-            aliquid sunt eos magni ab fugit tempora iure iusto deleniti, saepe
-            laborum impedit distinctio quia cupiditate at inventore quas sed
-            ipsam? Sint, maiores similique quae magni possimus mollitia rem vel
-            animi dicta porro dolores doloremque quasi, quas numquam, maxime
-            illo assumenda.
-          </p>
+          <span className="card-title">{project.title}-</span>
+          <p>{project.content}</p>
           <div className="card-action grey lighten-4 grey-text">
-            <div>Posted by Me</div>
+            <div>
+              Posted by {project.authorFName} {project.authorLName}
+            </div>
             <div>3rd of March, 2PM</div>
           </div>
         </div>
       </div>
     </div>
+  ) : (
+    <div className="container center">No such project!</div>
   );
 };
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    project: state.firestore.data.project,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect((props) => [
+    { collection: "projects", doc: props.match.params.id, storeAs: "project" },
+  ])
+)(ProjectDetails);
