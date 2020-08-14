4c46b7a53f46e2148b21f81972e7d1a73c895b7f
aslanchik <aslanchikgw2@hotmail.com>
2020-08-14 09:44:58 +0300

    added dummy data for development

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,18 @@
-const initState = {};
+const initState = {
+  projects: [
+    {
+      id: "1",
+      title: "projector",
+      content: "plan your projects in a nice and cohesive way",
+    },
+    { id: "2", title: "esti", content: "manage patients in a clinic/ER" },
+    {
+      id: "3",
+      title: "pokedex",
+      content: "gather pokemon and index them all!",
+    },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
