c984a632a9a2e18589c6afe9b9be2a6739dbf039
aslanchik <aslanchikgw2@hotmail.com>
2020-08-29 21:32:20 +0300

    deployed for the first time woohoo

---
+++ dist/precache-manifest.6ebbc3d3f581f6b525126540f92e5129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e487e19309cca86488044389b3a3384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce56dec5d9c52ad8f0b",
+    "url": "/static/css/2.7547a9ae.chunk.css"
+  },
+  {
+    "revision": "d4fd305da0b1bac20d4f",
+    "url": "/static/css/main.01df5cb9.chunk.css"
+  },
+  {
+    "revision": "8ce56dec5d9c52ad8f0b",
+    "url": "/static/js/2.3047e3cc.chunk.js"
+  },
+  {
+    "revision": "bc85e06215a04944aef1a729ad6ca5ad",
+    "url": "/static/js/2.3047e3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4fd305da0b1bac20d4f",
+    "url": "/static/js/main.da6fa502.chunk.js"
+  },
+  {
+    "revision": "785a0b8e37242cdf989f",
+    "url": "/static/js/runtime-main.dc34a4ff.js"
+  }
+]);
